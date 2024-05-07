c21efa9556ba8c6005d1aec75c17ed0accb0a32c
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-05-07 16:38:08 -0700

    adding task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,31 +6,13 @@ subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Retrieve the number of subscribers for a given subreddit.
-
-    Parameters:
-    subreddit (str): The name of the subreddit.
-
-    Returns:
-    int: The number of subscribers of the specified subreddit.
-         Returns 0 if the subreddit is invalid or the request fails.
-    """
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-
-    try:
-        # Make API request to get subreddit info
-        url = f'http://www.reddit.com/r/{subreddit}/about.json'
-        headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise exception for invalid responses
-        data = response.json()
-
-        # Extract subscriber count from response
-        subscribers = data.get("data", {}).get("subscribers", 0)
-        return subscribers
-    except requests.RequestException as e:
-        print(f"Error occurred while fetching data: {e}")
-        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
